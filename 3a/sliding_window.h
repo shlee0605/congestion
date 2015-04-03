a28ba81a67e909f0f0cb214cbeaeacab9fdc8df4
chcokr <chcokr@gmail.com>
2015-04-02 23:11:29 -0400

    Add enqueue_packet() and dequeue_packet()

--- 3a/sliding_window.h
+++ 3a/sliding_window.h
@@ -1,4 +1,13 @@
 #ifndef _CONGESTION_SLIDING_WINDOW_H_
 #define _CONGESTION_SLIDING_WINDOW_H_
 
+/// Returns the packet_t* in the front of the queue.
+/// Returns NULL if the queue is empty.
+packet_t *dequeue_packet();
+
+/// Enqueues a new packet to the end of the queue.
+/// The packet will be picked up by the Sliding Window algorithm
+/// as soon as possible.
+void enqueue_packet(packet_t *p_packet);
+
 #endif //_CONGESTION_SLIDING_WINDOW_H_
