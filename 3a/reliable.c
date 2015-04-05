f6e0754e823abba557bf95eac125523aca992fa5
yy69 <yy69@duke.edu>
2015-04-05 06:16:27 -0400

    Remove unnecessary includes

--- 3a/reliable.c
+++ 3a/reliable.c
@@ -1,17 +1,7 @@
-
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <assert.h>
-#include <poll.h>
-#include <errno.h>
-#include <time.h>
-#include <sys/time.h>
 #include <sys/socket.h>
-#include <sys/uio.h>
-#include <netinet/in.h>
 
 #include "reliable.h"
 
